c21dc5a759d82ee5d48732a81e30e50c1ec04c76
Snigdha Adhikari <snigdhaadhikari49@gmail.com>
2024-05-28 11:11:41 +0545

    Added Not Found Page using Bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Notfound from './pages/Notfound'
+import Home from './pages/Home'
 
 
 function App() {
@@ -13,7 +14,8 @@ function App() {
     <Routes>
       <Route path = "/Login" element = {<Login/>}/>
       <Route path = "/Register" element = {<Register/>}/>
-      <Route path = "/Notfound" element = {<Notfound/>}/>
+      <Route path = "/Home" element = {<Home/>}/>
+      <Route path = "*" element = {<Notfound/>}/>
       
     </Routes>
     </>
--- client/src/pages/Notfound.jsx
+++ client/src/pages/Notfound.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import {Link} from "react-router-dom"
 
-const Notfound = () => {
+const NotFound = () => {
   return (
-    <div>Notfound</div>
-  )
-}
+    <Container className="d-flex flex-column justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <Row>
+        <Col className="text-center">
+          <h1 className="display-1">404</h1>
+          <p className="lead">Oops! The page you are looking for does not exist.</p>
+          <Button variant="primary">  <Link to="/Home">Go back Home</Link></Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Notfound
+export default NotFound;
